18bd585229fade15168da9e53a71cd9af6446580
Matheus Cruz <matheuscruz.dev@gmail.com>
2023-09-03 13:16:52 -0300

    feat: use openapi-generator to generate k6 script

--- deployment/src/main/java/com/github/mcruzdev/quarkus/openapi/k6/generator/deployment/OpenApiK6GenProvider.java
+++ deployment/src/main/java/com/github/mcruzdev/quarkus/openapi/k6/generator/deployment/OpenApiK6GenProvider.java
@@ -4,15 +4,20 @@ import io.quarkus.bootstrap.prebuild.CodeGenException;
 import io.quarkus.deployment.CodeGenContext;
 import io.quarkus.deployment.CodeGenProvider;
 import org.eclipse.microprofile.config.Config;
+import org.openapitools.codegen.DefaultGenerator;
+import org.openapitools.codegen.config.CodegenConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.stream.Stream;
 
 public class OpenApiK6GenProvider implements CodeGenProvider {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(OpenApiK6GenProvider.class);
+    private static final String K6 = "k6";
 
     @Override
     public String providerId() {
@@ -31,8 +36,31 @@ public class OpenApiK6GenProvider implements CodeGenProvider {
 
     @Override
     public boolean trigger(CodeGenContext context) throws CodeGenException {
+        Path inputDir = context.inputDir();
+        Path outputDir = context.outDir();
 
+        try (Stream<Path> pathStream = Files.walk(inputDir)) {
+            pathStream
+                    .filter(Files::isRegularFile)
+                    .filter(path -> path.toString().endsWith(".json"))
+                    .forEach(path -> {
+                        CodegenConfigurator configurator = new CodegenConfigurator();
+                        configurator
+                                .setGeneratorName(K6)
+                                .setInputSpec(path.toString())
+                                .setOutputDir(outputDir.toString());
+                        DefaultGenerator generator = new DefaultGenerator();
+                        generator.opts(configurator.toClientOptInput()).generate();
+                    });
+
+        } catch (IOException e) {
+            throw new CodeGenException(e);
+        }
+
+        return false;
     }
+
+
 
     @Override
     public boolean shouldRun(Path sourceDir, Config config) {
